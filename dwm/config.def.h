24f717241e7b902b696d92291f91ca978425c7f8
Zowws <62174789+zowws@users.noreply.github.com>
2021-10-15 19:19:15 +0200

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -30,7 +30,7 @@ static const unsigned int alphas[][3]      = {
 /* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
 /* static const char *tags[] = { "", "", "", "", "", "", "", "", "" }; */
 /* static const char *tags[] = { "", "", "", "", "",  "", "", "", "", "" }; */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "", "", "" };
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -120,6 +120,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	TAGKEYS(                        XK_0,                      9)
+	TAGKEYS(                        XK_F1,                     10)	
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
