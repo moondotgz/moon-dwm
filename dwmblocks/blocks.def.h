12e665e21d0f5cc47f952107747e5f2f63c12c5a
Zowws <62174789+zowws@users.noreply.github.com>
2021-10-15 17:57:11 +0200

    Add files via upload

---
+++ dwmblocks/blocks.def.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{"", "date '+%b %d (%a) %I:%M%p'",					1,		0},
+ 
+    {"🗘", "uptime | awk '{print $1}'", 1, 0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
