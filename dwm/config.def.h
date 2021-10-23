321a1e7a1317d957598c7fac98ec8ced5e6aa1cd
Zowws <62174789+zowws@users.noreply.github.com>
2021-10-23 14:04:13 +0200

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -73,7 +73,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL }; 
 static const char *browser[]  = { "vivaldi ", NULL };
-static const char *dmenu2[]  = { "dmenu_run", NULL }; /* I use DistroTube's build */
+static const char *dmenu2[]  = { "dmenu_run", NULL }; 
 static const char *mail[]  = { "thunderbird", NULL }; /* Thunderbird */
 /* static const char *logout[] = { "arcolinux-logout", NULL };  */ /* Only uncomment this if you are using ArcoLinux https://arcolinux.com/ */
 
