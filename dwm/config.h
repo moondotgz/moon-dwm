e5edf101f8c9e8cd499c5c7681e94c54a6af4b4e
Simon Sudden <playermadc@gmail.com>
2021-11-06 11:00:58 +0100

    Change font size

--- dwm/config.h
+++ dwm/config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Mononoki Nerd Font Regular:size=10" };
-static const char dmenufont[]       = "Mononoki Nerd Font Regular:size=10";
+static const char *fonts[]          = { "Mononoki Nerd Font Regular:size=13" };
+static const char dmenufont[]       = "Mononoki Nerd Font Regular:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
